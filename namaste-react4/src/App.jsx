9f318548f7a9695096be020156782d662ee1c9c3
fazza-tech <ebranfazza@gmail.com>
2025-02-09 10:31:16 +0530

    added every file

---
+++ namaste-react4/src/App.jsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import {resList} from './utils/restaurentList'
+import swiggyLogo from './img/swiggy-logo.svg';
+
+/* Components of Our Food-Order App
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search Bar
+ * - Restaurant-Container
+ *  - Restaurant-Card
+ *    - Img
+ *    - Name of Res, Star Rating, cuisine, delivery time.
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src={swiggyLogo}
+          alt="App Logo"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+//  const styleCard = {
+//   backgroundColor: '#f0f0f0',
+// };
+
+// * Props :
+
+// * prop -> is Just a JS Object
+
+// * Note: When you have to dainamically pass in a data to a component, you pass in prop
+
+// const RestaurantCard = (props) => {
+// console.log(props);
+
+// * Note We can also destructure props on the fly by wrapping them in between {}, this is like...
+
+// * const { resName, cuisine } = props;
+
+// const RestaurantCard = ({ resName, cuisine }) => {
+//   console.log({ resName, cuisine });
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.info;
+
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: '#f0f0f0',
+      }}
+    >
+      <img
+        className="res-logo"
+        // src={
+        //   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/' +
+        //   resData.data.cloudinaryImageId
+        // }
+
+        src={
+          'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' +
+          cloudinaryImageId
+        }
+        alt="Biryani"
+      />
+      {/* <h3>{props.resName}</h3>
+        <h4>{props.cuisine}</h4> */}
+      {/* <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines.join(', ')}</h4>
+      <h4>{resData.data.avgRating} stars</h4>
+      <h4>₹{resData.data.costForTwo / 100} FOR TWO</h4>
+      <h4>{resData.data.deliveryTime} minutes</h4> */}
+      <h3>{name}</h3>
+      <h4>{cuisines.join(', ')}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+
+
+// * not using keys (not acceptable) <<<< index as a key <<<<<<<<<< unique id (is the best  practice)
+
+const Body = () => {
+
+  const cardMaping = resList.map((restaurant) => (
+    <RestaurantCard 
+    key={restaurant.info.id} 
+    resData={restaurant} />
+  ))
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input type="text" placeholder="Search Food or Restaurant" />
+        <button>Search</button>
+      </div>
+      <div className="res-container">
+
+        {/* // * looping through the <RestaurentCard /> components Using Array.map() method */}
+
+        {cardMaping}
+
+        {/* // * or */}
+
+        {/* // * We can also use index as the key to the JSX child elemnt - which is the 2nd parameter of the map() method, but is not a recommended practice - react official Docs declared this/}
+
+        {resList.map((restaurant, index) => (
+          <RestaurantCard key={index} resData={restaurant} />
+        ))}
+
+        {/* // * Why should we provide key property to the child elements - When creating a list in the UI from an array with JSX, you should add a key prop to each child and to any of its' children. React uses the key prop create a relationship between the component and the DOM element. The library uses this relationship to determine whether or not the component should be re-rendered.
+         */}
+      </div>
+    </div>
+  );
+};
+
+// * What is Config-driven-UI -> A "config-driven UI" is a user interface that is built and configured using a declarative configuration file or data structure, rather than being hardcoded.
+
+// * Every company now-a-days follows these approach, because our Appications need to be Dynamic These Days
+
+// * Note: A Good Senior Frontend engineer is - who is a good UI Layer Engineer and a good Data Layer Engineer
+
+const currYear = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Copyright &copy; {currYear}, Made with 💖 by <strong>Fasal</strong>
+      </p>
+    </footer>
+  );
+};
+
+function App  () {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+
+export default App
