b3801ffae32cf0eee61bc7cf60d54aea99b26c3c
fazza-tech <ebranfazza@gmail.com>
2025-02-11 21:43:32 +0530

    added useState

--- namaste-react4/src/components/Body.jsx
+++ namaste-react4/src/components/Body.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import {resList} from '../utils/restaurentList'
 import RestaurantCard from './RestaurentCard';
 
 
 const Body = () => {
 
-    const cardMaping = resList.map((restaurant) => (
+    const [restaurantList,  setRestaurantList] = useState(resList)
+
+    const cardMaping = restaurantList.map((restaurant) => (
       <RestaurantCard 
       key={restaurant.info.id} 
       resData={restaurant} />
@@ -18,29 +21,18 @@ const Body = () => {
         <div className='filter'>
             <button className='filter-btn' 
                     onClick={() => {
-                      const filteredRes = resList.filter(res => res.info.avgRating > 4);
-                      console.log(filteredRes); // Just logs the filtered list
+                      const filteredRes = restaurantList.filter(res => res.info.avgRating > 4);
+                      setRestaurantList(filteredRes)
                     }}>
               Top rated Restaurents
               </button>
 
         </div>
         <div className="res-container">
-  
-          {/* // * looping through the <RestaurentCard /> components Using Array.map() method */}
-  
           {cardMaping}
+          
   
-          {/* // * or */}
-  
-          {/* // * We can also use index as the key to the JSX child elemnt - which is the 2nd parameter of the map() method, but is not a recommended practice - react official Docs declared this/}
-  
-          {resList.map((restaurant, index) => (
-            <RestaurantCard key={index} resData={restaurant} />
-          ))}
-  
-          {/* // * Why should we provide key property to the child elements - When creating a list in the UI from an array with JSX, you should add a key prop to each child and to any of its' children. React uses the key prop create a relationship between the component and the DOM element. The library uses this relationship to determine whether or not the component should be re-rendered.
-           */}
+        
         </div>
       </div>
     );
