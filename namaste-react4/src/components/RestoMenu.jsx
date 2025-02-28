5e2e8f6c921085b8f1d0c39a0fe93eaf5e864bd9
fazza-tech <ebranfazza@gmail.com>
2025-02-28 21:12:54 +0530

    added RestroMenu Comp

---
+++ namaste-react4/src/components/RestoMenu.jsx
@@ -0,0 +1,17 @@
+const RestroMenu = () => {
+
+
+    return(
+        <div className="menu">
+            <h1>Name of the restaurent</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biriyani</li>
+                <li>Burgers</li>
+                <li>Diet Coke</li>
+            </ul>
+        </div>
+    )
+}
+
+export default RestroMenu
